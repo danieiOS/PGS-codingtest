d57744a1f41b12efbc960ce6710e88158358d0a9
Seongwook Song <danielosong98@gmail.com>
2024-01-04 17:50:34 +0900

    [PSG] LV_0 Day_1 update again
    [PSG] LV_0 Day_1 update again

---
+++ PGS-codingtest/PGS/LV_0/Day1.swift
@@ -0,0 +1,27 @@
+//
+//  Day1.swift
+//  PGS-codingtest
+//
+//  Created by 송성욱 on 1/4/24.
+//
+
+import Foundation
+//MARK: 두 수의 합
+func solution(_ num1: Int, _ num2: Int) -> Int {
+	return num1 + num2
+}
+
+//MARK: 두 수의 차
+func solution(_ num1: Int, _ num2: Int) -> Int {
+	return num1 - num2
+}
+
+//MARK: 두 수의 곱
+func solution(_ num1: Int, _ num2: Int) -> Int {
+	return num1 * num2
+}
+
+//MARK: 몫 구하기
+func solution(_ num1: Int, _ num2: Int) -> Int {
+	return num1 / num2
+}
